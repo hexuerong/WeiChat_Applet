049d26a73ce8490c42465106fcb78ce490fcdcb6
hexuerong-7f <hexuerong@7fgame.com>
2018-12-05 10:47:44 +0800

    http请求及https请求，上传文件等

--- server/httpsServer.js
+++ server/httpsServer.js
@@ -1,19 +1,25 @@
-// import fs from 'fs'
 const https = require('https');
 const fs = require('fs');
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+ 
+const app = express();
+const rootPath = path.join(__dirname,'../');
+// 创建 application/x-www-form-urlencoded 编码解析(post方法)
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 const options = {
     key: fs.readFileSync('bin/privatekey.pem'),
     cert: fs.readFileSync('bin/certificate.pem')
 };
 
-https.createServer(options, (req, res) => {
+/* https.createServer(options, (req, res) => {
     res.writeHead(200);
     res.end('hello world\n');
     console.log("应用实例，访问地址为 https://%s:%s", '127.0.0.1','8082');
 }).listen(8082);
-
-/* https.get('https://encrypted.google.com/', (res) => {
+https.get('https://encrypted.google.com/', (res) => {
   console.log('statusCode:', res.statusCode);
   console.log('headers:', res.headers);
 
@@ -25,9 +31,30 @@ https.createServer(options, (req, res) => {
   console.error(e);
 }); */
 
-https.get('https://127.0.0.1:8082/testHttps',res => {
-    console.log(res);
-    res.send('测试https\n');
-}).on('error', (e) => {
-    console.error(e);
+const httpsServer = https.createServer(options,app);
+httpsServer.listen(8082, function() {
+    console.log('HTTPS Server is running on: https://127.0.0.1:%s', 8082);
+});
+
+app.get('/',(req,res) => {
+    // res.send('Hello World');
+    res.sendFile( rootPath + "/page_testServer/" + "https.html" );
+});
+app.get('/testHttps',(req,res) => {
+    // 输出 JSON 格式
+    let response = {
+        "name":req.query.name,
+        "password":req.query.password
+    };
+    console.log(response);
+    res.end(JSON.stringify(response));
+});
+app.post('/process_post', urlencodedParser, function (req, res) {
+    // 输出 JSON 格式
+    let response = {
+        "name":req.body.name,
+        "password":req.body.password
+    };
+    console.log(response);
+    res.end(JSON.stringify(response));
 });
--- server/server.js
+++ server/server.js
@@ -1,5 +1,14 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const multer  = require('multer');
+const bodyParser = require('body-parser');
+
 const app = express();
+const rootPath = path.join(__dirname,'../');
+
+app.use(multer({ dest: '/tmp/'}).array('image'));//上传文件
+app.use(bodyParser.urlencoded({ extended: false }));
 
 const server = app.listen(8081,'127.0.0.1',function(){
     var host = server.address().address
@@ -11,10 +20,37 @@ const server = app.listen(8081,'127.0.0.1',function(){
 app.get('/', function (req, res) {
     res.send('Hello World');
 });
+app.get('/page_testServer/http.html', function (req, res) {//返回页面
+    res.sendFile( rootPath + "/page_testServer/" + "http.html" );
+});
 
 app.get('/onLogin', function (req, res) {
-    // res.send(req);
-    res.send('---------------------------------------');
-    // res.send(res);
-    console.log(res);
+    // 输出 JSON 格式
+    let response = {
+        "name":req.query.name,
+        "password":req.query.password
+    };
+    console.log(response);
+    res.end(JSON.stringify(response));
+});
+
+app.post('/file_upload', function (req, res) {
+ 
+    console.log(req.files[0]);  // 上传的文件信息
+  
+    let des_file = __dirname + "/" + req.files[0].originalname;
+    fs.readFile( req.files[0].path, function (err, data) {
+         fs.writeFile(des_file, data, function (err) {
+          if( err ){
+               console.log( err );
+          }else{
+                response = {
+                    message:'File uploaded successfully', 
+                    filename:req.files[0].originalname
+               };
+           }
+           console.log( response );
+           res.end( JSON.stringify( response ) );
+        });
+    });
 });
